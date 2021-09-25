f1b66144a1bdf0df71bd242138143a5d9c2ac170
velaztef <velazquezstefania@gmail.com>
2021-09-25 12:50:00 -0300

    Agrego Navbar y React-Bootstrap.

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+
+function NavBar () {
+    return (
+    
+        <Navbar bg="dark" variant="dark">
+          <Container>
+          <Navbar.Brand href="#home">E-commerce</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          </Container>
+        </Navbar>
+    )
+}
+
+export default NavBar 
